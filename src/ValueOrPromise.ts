365a5a681759f9d3302d31bd2c06f64772d86a78
Laurin Quast <laurinquast@googlemail.com>
2021-05-28 10:35:32 +0200

    feat: remove forced nullability + overload ValueOrPromise.all

--- src/ValueOrPromise.ts
+++ src/ValueOrPromise.ts
@@ -6,7 +6,7 @@ function isPromiseLike<T>(object: unknown): object is PromiseLike<T> {
 
 interface FulfilledState<T> {
   status: 'fulfilled';
-  value: T | undefined | null;
+  value: T;
 }
 
 interface RejectedState {
@@ -28,8 +28,8 @@ const defaultOnRejectedFn = (reason: unknown) => {
 export class ValueOrPromise<T> {
   private readonly state: State<T>;
 
-  constructor(executor: () => T | PromiseLike<T> | undefined | null) {
-    let value: T | PromiseLike<T> | undefined | null;
+  constructor(executor: () => T | PromiseLike<T>) {
+    let value: T | PromiseLike<T>;
 
     try {
       value = executor();
@@ -106,10 +106,28 @@ export class ValueOrPromise<T> {
     return state.value;
   }
 
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>, ValueOrPromise<T6>, ValueOrPromise<T7>, ValueOrPromise<T8>, ValueOrPromise<T9>, ValueOrPromise<T10>]): ValueOrPromise<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5, T6, T7, T8, T9>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>, ValueOrPromise<T6>, ValueOrPromise<T7>, ValueOrPromise<T8>, ValueOrPromise<T9>]): ValueOrPromise<[T1, T2, T3, T4, T5, T6, T7, T8, T9]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5, T6, T7, T8>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>, ValueOrPromise<T6>, ValueOrPromise<T7>, ValueOrPromise<T8>]): ValueOrPromise<[T1, T2, T3, T4, T5, T6, T7, T8]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5, T6, T7>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>, ValueOrPromise<T6>, ValueOrPromise<T7>]): ValueOrPromise<[T1, T2, T3, T4, T5, T6, T7]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5, T6>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>, ValueOrPromise<T6>]): ValueOrPromise<[T1, T2, T3, T4, T5, T6]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4, T5>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>, ValueOrPromise<T5>]): ValueOrPromise<[T1, T2, T3, T4, T5]>;
+  // prettier-ignore
+  public static all<T1, T2, T3, T4>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>, ValueOrPromise<T4>]): ValueOrPromise<[T1, T2, T3, T4]>;
+  // prettier-ignore
+  public static all<T1, T2, T3>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>, ValueOrPromise<T3>]): ValueOrPromise<[T1, T2, T3]>;
+  // prettier-ignore
+  public static all<T1, T2>(values: readonly [ValueOrPromise<T1>, ValueOrPromise<T2>]): ValueOrPromise<[T1, T2]>;
   public static all<T>(
     valueOrPromises: ReadonlyArray<ValueOrPromise<T>>
-  ): ValueOrPromise<Array<T | null | undefined>> {
-    const values: Array<T | null | undefined> = [];
+  ): ValueOrPromise<Array<T>> {
+    const values: Array<T> = [];
 
     for (let i = 0; i < valueOrPromises.length; i++) {
       const valueOrPromise = valueOrPromises[i];
